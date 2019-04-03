3ca37f5874f9d2ec29afd84442462a643f390c1d
steelbridge-io <chris@steelbridge.io>
2019-04-02 20:52:04 -0700

    first commit -> /customizer-css/customizer-css.php

---
+++ library/customizer-css/customizer-css.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chrisparsons
+ * Date: 2019-02-27
+ * Time: 10:55
+ */
+
+
+
+
+#top-cta, #top-cta h2, #top-cta p
+
+// CSS output for cutomizer CSS modifications
+if ( is_customize_preview() ) {
+
+	function output()
+	{
+		echo '<style>';
+
+		$fptopbgcolor_color = get_theme_mod('fp-top-widget-bg-color');
+		$fptoptxtcolor			= get_theme_mod('fp-top-widget-txt-color');
+		echo '#top-cta {
+						background-color: ' . $fptopbgcolor_color . ';
+						}' .
+					'#top-cta, #top-cta h2, #top-cta p {
+						color: ' . $fptoptxtcolor . ';
+						}';
+
+		echo '</style>';
+	}
+
+	add_action('wp_head', 'output');
+}
